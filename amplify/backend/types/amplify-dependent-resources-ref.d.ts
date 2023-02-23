bf9d60ef414f7526511ba9162d19b1b436cf0b6a
rickzen <rickzen9x@gmail.com>
2023-02-23 13:32:48 +0200

    added database and api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactapp772520c0": {
       "AppClientID": "string",
